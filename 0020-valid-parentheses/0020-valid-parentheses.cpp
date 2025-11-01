e791002bc78bf273f418a057bd29978913578cb9
Rishabh Choraria <169180171+Rish-Hub05@users.noreply.github.com>
2025-11-01 15:56:49 +0530

    Time: 0 ms (100%), Space: 8.8 MB (87.24%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{'|| s[i]=='[') st.push(s[i]);
+            else{
+                if(st.empty()) return false;
+                char topi=st.top();
+                st.pop();
+                if( (s[i]==')'&&topi=='(') || (s[i]=='}'&&topi=='{') || (s[i]==']'&&topi=='['))
+                    continue;
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
