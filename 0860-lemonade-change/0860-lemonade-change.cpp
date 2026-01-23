9421058b74855a5337ca102f6ccec82ba5c96a07
Rishabh Choraria <169180171+Rish-Hub05@users.noreply.github.com>
2026-01-23 18:02:26 +0530

    Time: 0 ms (100%), Space: 87.1 MB (94.56%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int b5=0,b10=0,b20=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5) b5++;
+            else if(bills[i]==10){
+                if(b5==0) return false;
+                b10++;
+                b5--;
+            }
+            else if(bills[i]==20){
+                if(b10>0&&b5>0){
+                    b5--;
+                    b10--;
+                }
+                else if(b5>=3) b5-=3;
+                else return false;
+            }
+        }
+        return true;
+    }
+};
