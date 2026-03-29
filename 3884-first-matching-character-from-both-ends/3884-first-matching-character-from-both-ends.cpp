17a8bf09cd8d6fe8343e0ba7908b0b6183f3d42e
Rishabh Choraria <169180171+Rish-Hub05@users.noreply.github.com>
2026-03-29 23:49:43 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.96%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        int n=s.size();
+        int l=0,r=n-1;
+        while(l<=r){
+            if(s[l]==s[r])
+            return l;
+            l++;
+            r--;
+        }
+        return -1;
+    }
+};
